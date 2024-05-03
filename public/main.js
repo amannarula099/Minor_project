d700ee29af4983acf23bb5a62a274e1a3bacafa5
amannarula099 <163774574+amannarula099@users.noreply.github.com>
2024-05-03 23:10:53 +0530

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,51 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+
+            });
+        };
+    });
+};
+
+let slides = document.getElementsByClassName('review')[0].getElementsByClassName('flex')[0].getElementsByClassName('slides-container')[0].getElementsByClassName('slide');
+let index = 0;
+
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+gsap.from(".img_svg", {
+    x:700,
+    duration:1
+})
+
+gsap.from(".content", {
+    x:-700,
+    duration:1
+})
+
+
+
+
+
+
